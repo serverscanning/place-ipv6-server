aa465fc3e956d9105cc8fa7aa693815e8aadc7ca
EnderKill98 <no@mail.tld>
2023-06-06 00:47:11 +0200

    Add simple pps display

--- static/script.js
+++ static/script.js
@@ -4,9 +4,11 @@ function subscribeToCanvas() {
     const canvasEl = document.getElementById("canvas");
     const canvasCtx = canvasEl.getContext("2d");
     const canvasStatusEl = document.getElementById("canvas-status");
+    const canvasPpsEl = document.getElementById("canvas-pps");
 
     console.log("Websocket: Connecting...");
     canvasStatusEl.innerText = "Connecting...";
+    canvasPpsEl.innerText = "";
 
     const ws = new WebSocket((document.location.protocol === "https:" ? "wss://" : "ws://") + document.location.host + "/ws");
     ws.binaryType = "blob";
@@ -16,6 +18,7 @@ function subscribeToCanvas() {
 
         ws.send(JSON.stringify({ request: "delta_canvas_stream", enabled: true }));
         ws.send(JSON.stringify({ request: "get_full_canvas_once" }));
+        ws.send(JSON.stringify({ request: "pps_updates", enabled: true }));
     };
 
     const visibilityChangeHandler = (event) => {
@@ -41,12 +44,42 @@ function subscribeToCanvas() {
         document.removeEventListener("visibilitychange", visibilityChangeHandler);
         console.log("Websocket: Closed. Reconnecting in 3s...");
         canvasStatusEl.innerText = (didError ? "Error!" : "Lost connection!") + " Attempting to reconnect in 3s...";
+        canvasPpsEl.innerText = "";
         setTimeout(subscribeToCanvas, 3000);
     }
 
+    let ppsEntries = [];
+
     ws.onmessage = async (event) => {
-        if (!(event.data instanceof Blob)) return; // Receiving text is not supported rn (pps later?)
-        let imageBitmap = await createImageBitmap(event.data);
-        canvasCtx.drawImage(imageBitmap, 0, 0);
+        if (event.data instanceof Blob) {
+            let imageBitmap = await createImageBitmap(event.data);
+            canvasCtx.drawImage(imageBitmap, 0, 0);
+        } else if(typeof(event.data) === "string") {
+            let wsMessage = JSON.parse(event.data);
+            if (wsMessage.message === "pps_update") {
+                ppsEntries.push(wsMessage.pps);
+                while (ppsEntries.length > 10) ppsEntries.splice(0, 1);
+                let maxPps = 0;
+                for (const pps of ppsEntries) if (pps > maxPps) maxPps = pps;
+                canvasPpsEl.innerText = "PPS (current / max in last 30s): " + formatNumber(wsMessage.pps, digits(maxPps)) + " / " + formatNumber(maxPps, 0);
+            }
+        } else {
+            console.error("Received invalid type ws data: " + typeof (event.data));
+        }
     }
+}
+
+function digits(number) {
+    return Math.floor(Math.log10(number) + 1);
+}
+
+function formatNumber(number, padToMinDigits) {
+    let numberStrRev = (number + "").split("").reverse().join("");
+    while (numberStrRev.length < padToMinDigits) numberStrRev += "0";
+    let newNumberStrRev = "";
+    for (const i in numberStrRev) {
+        if (i > 0 && i % 3 == 0) newNumberStrRev += ",";
+        newNumberStrRev += numberStrRev[i];
+    }
+    return newNumberStrRev.split("").reverse().join("");
 }
